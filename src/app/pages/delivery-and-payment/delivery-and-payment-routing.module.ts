816751aed0133c39e5ec497935fc6c23f4c02fc6
SerhiiChorpita <sergiochorpita@gmail.com>
2023-03-30 17:30:49 -0600

    delivery page 1/2

--- src/app/pages/delivery-and-payment/delivery-and-payment-routing.module.ts
+++ src/app/pages/delivery-and-payment/delivery-and-payment-routing.module.ts
@@ -5,7 +5,7 @@ import { DeliveryAndPaymentComponent } from './delivery-and-payment.component';
 
 const routes: Routes = [
   {
-    path: 'delivery-and-payment',
+    path: '',
     component: DeliveryAndPaymentComponent
   }
 ]
